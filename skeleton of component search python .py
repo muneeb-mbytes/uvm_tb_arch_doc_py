312e6a07030fb7b787f15b34a599353bb8300902
Ashika Ajai <76145955+AshikaAjai@users.noreply.github.com>
2021-02-19 10:35:17 +0530

    Update skeleton of component search python .py

--- skeleton of component search python .py
+++ skeleton of component search python .py
@@ -23,17 +23,17 @@ def component_search(keyword):  # to search for keyword in all files of director
                             break
                         else: break
 def tb_comps():  #generic word in tb components that can be searched to determine presence of comp in TB
-    component_search("uvm_test")
-    component_search("uvm_env")
-    component_search("uvm_scoreboard")
-    component_search("uvm_agent")
-    component_search("uvm_driver")
-    component_search("uvm_monitor")
-    component_search("uvm_sequencer")
-    component_search("uvm_sequence")
-    component_search("uvm_sequence_item")
-    component_search("interface")
-    component_search("package")
+    component_search("uvm_test")  #search for keyword uvm_test
+    component_search("uvm_env")  #search for keyword uvm_env
+    component_search("uvm_scoreboard")  #search for keyword uvm_scoreboard
+    component_search("uvm_agent")  #search for keyword uvm_agent
+    component_search("uvm_driver")  #search for uvm_driver
+    component_search("uvm_monitor")  #search for uvm_monitor
+    component_search("uvm_sequencer")  #search for uvm_sequencer
+    component_search("uvm_sequence")  #search for uvm_sequence
+    component_search("uvm_sequence_item")  #search for uvm_sequence_item
+    component_search("interface")  #search for interface
+    component_search("package")  #search for package
 def rectangle(x,y,l,b,title):  ## draw rectangle with inputs co-ordinates , dimentions text
     t.goto(x,y)  #co-ordinates to go to
     t.penup()  #pen movement without write
@@ -42,86 +42,86 @@ def rectangle(x,y,l,b,title):  ## draw rectangle with inputs co-ordinates , dime
     t.pensize(5)  #pen thickness
     t.pendown()  #start draw
     for i in range(2): #draw rectangle with dimentions
-        t.forward(l)
-        t.right(90)
-        t.forward(b)
-        t.right(90)
-    t.penup()
+        t.forward(l)  # turtle move forward length
+        t.right(90)  #turtle turn right 90
+        t.forward(b)  #turtle forward breadth
+        t.right(90)  #turtle turn right 90
+    t.penup()  #stop writing
     t.goto(x+20, y-20)  # new value for text
-    t.pendown()
+    t.pendown() #start writing
     t.write(title, align="left", font=("Arial", "10", "bold"))  #text to be written , font size alignment type
-    t.penup()
+    t.penup() #stop writing
 def top():
-    t.penup()
-    rectangle(-700,380,1400,700,"top")
+    t.penup() 
+    rectangle(-700,380,1400,700,"top")  #rectangle x,y co-ordinates, length, breadth, title name
 def test():
     t.penup()
-    rectangle(-670,350,1350,650,"test")
+    rectangle(-670,350,1350,650,"test")  #rectangle x,y co-ordinates, length, breadth, title name
 def env():
     t.penup()
-    rectangle(-640,320,1300,600,"env")
+    rectangle(-640,320,1300,600,"env")  #rectangle x,y co-ordinates, length, breadth, title name
 def scoreboard():
     t.penup()
-    rectangle(30,300,620,50,"scoreboard")
+    rectangle(30,300,620,50,"scoreboard")  #rectangle x,y co-ordinates, length, breadth, title name
 def agent(x,agent_number):
-    y = (x) * 700*(2/agent_number)
-    t.goto(-620 + y, 200)
+    y = (x) * 700*(2/agent_number)  #y co-ordinates depends on number of agents
+    t.goto(-620 + y, 200)  # x y co-ordinates
     t.pendown()
-    for i in range(2):
+    for i in range(2): #  draw retangle
         t.forward(900/(agent_number))
         t.right(90)
         t.forward(450)
         t.right(90)
     t.penup()
-    t.goto(-630 + y, 210)
+    t.goto(-630 + y, 210)  #x y co-ordinates for title
     t.pendown()
     t.write("Agent " + str(x+1), align="left", font=("Arial", "10", "bold"))
     #t.color("white")
     t.penup()
-    sequencer(x,agent_number)
-    driver(x,agent_number)
-    monitor(x,agent_number)
+    sequencer(x,agent_number) #  call independent sequencer
+    driver(x,agent_number)  # call independent driver
+    monitor(x,agent_number)  #call independent monitor
 def sequencer(x,agent_number):
-    y = (x) * 700 * (2 / agent_number)
+    y = (x) * 700 * (2 / agent_number)  #y co-ordinates depends on number of agents
     t.goto(-610 + y, 160)
     t.pendown()
-    for i in range(2):
+    for i in range(2): #  draw retangle
         t.forward(800 / (agent_number))
         t.right(90)
         t.forward(80)
         t.right(90)
     t.penup()
-    t.goto(-610 + y, 170)
+    t.goto(-610 + y, 170)  #x y co-ordinates for title
     t.pendown()
     t.write("Sequencer " + str(x + 1), align="left", font=("Arial", "10", "bold"))
     #t.color("white")
     t.penup()
-def driver(x,agent_number):
-    y = (x) * 700 * (2 / agent_number)
+def driver(x,agent_number):  
+    y = (x) * 700 * (2 / agent_number)  #y co-ordinates depends on number of agents
     t.goto(-610 + y, 20)
     t.pendown()
-    for i in range(2):
+    for i in range(2): #  draw retangle
         t.forward(800 / (agent_number))
         t.right(90)
         t.forward(80)
         t.right(90)
     t.penup()
-    t.goto(-610 + y, 30)
+    t.goto(-610 + y, 30)  #x y co-ordinates for title
     t.pendown()
     t.write("Driver " + str(x + 1), align="left", font=("Arial", "10", "bold"))
     #t.color("white")
     t.penup()
-def monitor(x,agent_number):
+def monitor(x,agent_number):  #y co-ordinates depends on number of agents
     y = (x) * 700 * (2 / agent_number)
     t.goto(-610 + y, -140)
     t.pendown()
-    for i in range(2):
+    for i in range(2): #  draw retangle
         t.forward(800 / (agent_number))
         t.right(90)
         t.forward(80)
         t.right(90)
     t.penup()
-    t.goto(-610 + y, -130)
+    t.goto(-610 + y, -130)  #x y co-ordinates for title
     t.pendown()
     t.write("Monitor " + str(x + 1), align="left", font=("Arial", "10", "bold"))
     #t.color("white")
@@ -131,11 +131,11 @@ def uvm_tb(file_path):
     #print(file_path)
     f = open(file_path, "r") #opens package
     if f.mode == "r":
-        package = f.read()
+        package = f.read()  #reads the package file
        # print(package)
-        if(re.search("..env.sv",package)):
+        if(re.search("..env.sv",package)):  #searches for env in package
             #print("top test env")
-            top()
+            top()  # calls function to draw top test env and scoreboard.
             test()
             env()
             scoreboard()
@@ -147,13 +147,13 @@ def uvm_tb(file_path):
 
     f.close()
 tb_comps()
-def dump_gui():
-    print('...dumping gui window to png')
-    x0 = root.winfo_rootx()
+def dump_gui():  #image capture from canvas.
+    print('...dumping gui window to png') 
+    x0 = root.winfo_rootx()  #image capture size 
     y0 = root.winfo_rooty()
     x1 = x0 + root.winfo_width()*1.27
     y1 = y0 + root.winfo_height()*1.2
-    ImageGrab.grab().crop((x0, y0, x1, y1)).save("gui_image_grabbed.png")
+    ImageGrab.grab().crop((x0, y0, x1, y1)).save("gui_image_grabbed.png")  #image capture function and image naming
 dump_gui()
 t.exitonclick()
 #########
