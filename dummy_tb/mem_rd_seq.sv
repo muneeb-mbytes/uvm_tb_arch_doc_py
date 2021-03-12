d7563dcc7b54507a42ffa9b40875ed26dc1d45f1
navneetkhalsa <navneet.khalsa95@gmail.com>
2021-03-12 09:32:35 +0530

    Read seq in dummy_tb

---
+++ dummy_tb/mem_rd_seq.sv
@@ -0,0 +1,14 @@
+class mem_rd_seq extends uvm_sequence#(mem_seq_item);
+   
+  `uvm_object_utils(mem_rd_seq)
+    
+  //Constructor
+  function new(string name = "mem_rd_seq");
+    super.new(name);
+  endfunction
+   
+  virtual task body();
+    `uvm_do_with(req,{req.rd_en == 1;})
+  endtask
+   
+endclass
